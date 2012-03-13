4e3971b4aec88d126545fd33d484dcb223408679
BoDVa <dmitry@thebodva.com>
2012-03-13 23:15:39 +0200

    modify: change link to index

--- www/template/parts/header.php
+++ www/template/parts/header.php
@@ -1,5 +1,5 @@
 <?php if (!defined('defSimpleSite')) {die("Use site core!");}  ?>
 
 <div id="header" class="header">
-	<h1>&lt;?php echo 'Hello word!'<a href="/">;</a> <a href="?act=about">?</a>&gt;</h1>
+	<h1>&lt;?php echo 'Hello word!'<a href="./">;</a> <a href="?act=about">?</a>&gt;</h1>
 </div>
